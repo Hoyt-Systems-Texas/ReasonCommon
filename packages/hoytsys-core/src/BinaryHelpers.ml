ffd85557d008019bb1a8a1a269562d8db79a5269
Matt Hoyt <mrhoyt4@gmail.com>
2021-01-30 13:46:28 -0600

    Added code for converting a string buffer to a raw array.

--- packages/hoytsys-core/src/BinaryHelpers.ml
+++ packages/hoytsys-core/src/BinaryHelpers.ml
@@ -6,8 +6,21 @@ external make_buffer : Fetch.arrayBuffer -> uint8array = "Uint8Array" [@@bs.new]
 external make_text_decoder:  string -> text_decoder = "TextDecoder" [@@bs.new]
 external decode_text : text_decoder -> uint8array -> string = "decode" [@@bs.send]
 
+external get_buffer_source : Js_typed_array2.Int8Array.t -> Fetch.bufferSource= "buffer" [@@bs.get]
+
 (** Takes in the array buffer and returns a string that hasn't been modified at all.*)
 let to_raw_string buffer =
   let raw_buffer = make_buffer buffer in
   let decoder = make_text_decoder "x-user-defined" in
   decode_text decoder raw_buffer
+
+let to_buffer value =
+  let module A = Js_typed_array2.Int8Array in
+  let length = String.length value in
+  let raw_buffer = A.fromLength length in
+  let set_buffer = A.unsafe_set raw_buffer in
+  String.iteri (fun idx s ->
+    set_buffer idx (Char.code s)) value;
+  raw_buffer
+    |> get_buffer_source
+
--- packages/hoytsys-core/src/BinaryHelpers.mli
+++ packages/hoytsys-core/src/BinaryHelpers.mli
@@ -1,2 +1,5 @@
 (** Takes a fetch blob and converts it to a raw binary string. *)
 val to_raw_string: Fetch.arrayBuffer -> string
+
+(** Takes a binary string and converts to to a aarray buffer with out any changes. *)
+val to_buffer: string -> Fetch.bufferSource
